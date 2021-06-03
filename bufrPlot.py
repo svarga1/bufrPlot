f7dc3d5be3f7da8eb44a95ad584dfb46058aebf3
svarga <svarga1@terpmail.umd.edu>
2021-06-03 11:06:05 -0400

    "Version 1: Reading the data"

---
+++ bufrPlot.py
@@ -0,0 +1,30 @@
+#Program to plot Radiosonde bufr file
+
+#Modules
+import ncepbufr
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Open the file
+bufr = ncepbufr.open(filePath)
+bufr.advance()
+bufr.advance()
+bufr.advance() #Has to advance three times: load subset fails if less than three, and causes a crash if it advances more than three times. 5 times also works
+#Update: it no longer crashes if it advances more than three times. Might have been caused by advancing after loading
+bufr.load_subset()
+
+#Variables
+dryBulb=bufr.read__subset('TMDB').squeeze()-273.15 #Converts from K to C
+pressure=bufr.read_subset('PRLC').squeeze()/100 #Converts from Pa to hPa
+
+#Location and Date
+lon=bufr.read_subset('CLON')[0][0]
+lat=bufr.read_subset('CLAT')[0][0]
+date=bufr.msg_date
+
+bufr.close() #Closes the file
+
+print(np.shape(dryBulb))
+print(np.shape(pressure))
+print(lon)
+print(lat)
